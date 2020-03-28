0e819312ea1a81f22ea97278dba36e5340ecd31b
vamsivammu <vammuvamsi64@gmail.com>
2020-03-28 16:25:26 +0530

    smal changes

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://vzbbqzpfpolrsc:4c98c089c3d79905dfe1f8fe496e466e893d32ffb7842b22fa4f1f5dad744ac8@ec2-54-197-48-79.compute-1.amazonaws.com:5432/d8mgai0jqu28fs'
+const pool = new Pool({
+  connectionString: connectionString,
+})
+const express = require('express')
+const bp = require('body-parser')
+
+const app = express()
+
+app.use(bp.json())
+app.post('/',(req,res)=>{
+    pool.query('SELECT * FROM Persons',(err,res1)=>{
+        console.log(err,res1)
+        res.json(res1)
+    })
+})
+app.listen(3000,()=>{
+    console.log("listening")
+})
